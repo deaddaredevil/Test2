e33e2a97c555d586239f81321380b5727da3e913
deaddaredevil <113775017+deaddaredevil@users.noreply.github.com>
2023-05-13 15:33:30 +0300

    init

---
+++ Test2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace test
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "Evtushenko";
+            string famileName = "Valentin";
+            Console.WriteLine(name + " " + famileName);
+            string trueName;
+            string trueFamilyName;
+            trueName = famileName;
+            trueFamilyName = name;
+            Console.WriteLine(trueName + " " + trueFamilyName);
+
+        }
+    }
+}

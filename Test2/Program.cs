6d70745f1e958c4530f8237224c2a482aab0f202
deaddaredevil <113775017+deaddaredevil@users.noreply.github.com>
2023-05-14 14:46:18 +0300

    init

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -5,11 +5,11 @@
         static void Main()
         {
             int imagesCount = 52;
-            int picturesInRanges = 3;
+            int picturesInRange = 3;
             int fullRanges;
-            fullRanges = imagesCount / picturesInRanges;
+            fullRanges = imagesCount / picturesInRange;
             int extraPictures;
-            extraPictures = imagesCount % picturesInRanges;
+            extraPictures = imagesCount % picturesInRange;
             Console.WriteLine($"У вас {fullRanges} заполненых рядов и {extraPictures} у вас лишние!");
         }
     }
